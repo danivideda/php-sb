d72f9f1e0dd2e2e2bd46afbe62013486706c66c5
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-05-14 13:51:27 +0700

    add template index view

--- index.php
+++ index.php
@@ -1,43 +1,5 @@
 <?php
 
+$greeting = "Hello, world!";
 
-// class Person
-// {
-//   public function myMethod($name)
-//   {
-//     return "Hey {$name}! いらっしゃい\n";
-//   }
-// }
-
-// $hello = new Person();
-// $name = "Dani";
-// echo $hello->myMethod($name);
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Sandbox</title>
-  <style>
-    h1 {
-      background-color: #333;
-      padding: 2em;
-      text-align: center;
-      color: white;
-    }
-  </style>
-</head>
-<body>
-    <header>
-      <h1>
-
-        <?php 
-          echo "Hello, " . htmlspecialchars($_GET['name']);
-        ?>
-
-      </h1>
-    </header>
-</body>
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// class Person
+// {
+//   public function myMethod($name)
+//   {
+//     return "Hey {$name}! いらっしゃい\n";
+//   }
+// }
+
+// $hello = new Person();
+// $name = "Dani";
+// echo $hello->myMethod($name);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Sandbox</title>
+  <style>
+    h1 {
+      background-color: #333;
+      padding: 2em;
+      text-align: center;
+      color: white;
+    }
+  </style>
+</head>
+<body>
+    <header>
+      <h1>
+        <?= $greeting; ?>
+      </h1>
+    </header> 
+</body>
+</html>
