5813c0de3344b1bc4dd2db15919e1f8f8a27553a
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-05-18 10:54:41 +0700

    Init

--- index.php
+++ index.php
@@ -1,49 +1,13 @@
 <?php
 
 require 'functions.php';
-require 'task.php';
+require 'databases/Connection.php';
+require 'databases/QueryBuilder.php';
+require 'Task.php';
 
-$hobbies = [
-  'Play Soccer',
-  'Reading books',
-  'Travel'
-];
+$pdo = Connection::make();
+$query = new QueryBuilder($pdo);
 
-$person = [
-  'age' => 20,
-  'hair' => 'brown',
-  'career' => 'web developer'
-];
-
-$task_p = [
-  'title' => 'Finish homework',
-  'due' => 'today',
-  'assigned_to' => 'Dani',
-  'completed' => false
-]; // title, due, assigned_to, completed
-
-// class Task
-// {
-//   protected $description;
-//   protected $completed = false;
-
-//   public function __construct($description)
-//   {
-//     $this->description = $description;
-//   }
-
-//   public function isComplete()
-//   {
-//     return $this->completed;
-//   }
-
-//   public function complete()
-//   {
-//     $this->completed = true;
-//   }
-// }
-
-$pdo = connectToDb();
-$tasks = fetchAllTask($pdo);
+$tasks = $query->selectAll("todos");
 
 require 'index.view.php';
