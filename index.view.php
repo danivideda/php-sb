3ee1910aaa774da825dd42ca45b4222c71f97789
Dani Ihza Farrosi <dani.videda@gmail.com>
2020-05-18 10:55:32 +0700

    add previous tasklist html

--- index.view.php
+++ index.view.php
@@ -18,7 +18,18 @@
       <h1>Hello World!</h1>
     </header> 
 
-    
+    <h4>Task list from db</h4>
+    <ul>
+        <?php foreach ($tasks as $task) : ?>
+          <li>
+            <?php if ($task->complete) : ?>
+              <strike><strong><?= $task->description ?></strong></strike>
+            <?php else : ?>
+              <?= $task->description ?>
+            <?php endif ?>
+          </li>
+        <?php endforeach ?>
+    </ul>
    
 </body>
 </html>
